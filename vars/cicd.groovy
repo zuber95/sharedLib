0ed32fc3432dbc7f8c7d713b648dd412f2aa0846
zuber95 <32728868+zuber95@users.noreply.github.com>
2022-09-05 16:59:31 +0530

    Update cicd.groovy

--- vars/cicd.groovy
+++ vars/cicd.groovy
@@ -1,1 +1,19 @@
 
+def newGit(repo)
+{
+  git "${repo}"
+}
+def newMaven()
+{
+  sh 'mvn package'
+}
+def newDeploy(ip,appname)
+{
+  deploy adapters: [tomcat9(credentialsId: '898c13c4-28fd-4283-82d8-c07f78f8a5e8', path: '', url: "${ip}")], contextPath: "${appname}", war: '**/*.war'
+}
+
+def newTest(jobname)
+{
+  echo "${jobname}"
+  sh "java -jar /home/ubuntu/.jenkins/workspace/${jobname}/testing.jar"
+}
